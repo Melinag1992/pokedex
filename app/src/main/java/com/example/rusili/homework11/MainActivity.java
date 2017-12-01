4aab303cf817c70d1734f781eff6c4f4bd171522
Melina Gonzalez <melinag1992@gmail.com>
2017-12-01 14:26:48 -0500

    first commit

--- app/src/main/java/com/example/rusili/homework11/MainActivity.java
+++ app/src/main/java/com/example/rusili/homework11/MainActivity.java
@@ -1,8 +1,20 @@
 package com.example.rusili.homework11;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
 /**
  * Created by melina.gonzalez on 11/29/17.
  */
 
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitymain);
+
+        RecyclerView pokeRecyclerView = (RecyclerView) findViewById(R.id.poke_recyclerview);
+    }
 }
